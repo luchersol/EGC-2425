03cd644421d79ba829997db37fc14d3991544d5e
Lucas Manuel Herencia Solís <lmherencia2003@gmail.com>
2025-01-10 17:08:37 +0100

    Pruebas

--- app/modules/auth/tests/test_unit.py
+++ app/modules/auth/tests/test_unit.py
@@ -88,6 +88,22 @@ def test_service_create_with_profie_success(clean_database):
     assert UserRepository().count() == 1
     assert UserProfileRepository().count() == 1
 
+def test_service_incrementar_numero_usuario(clean_database):
+    count_user_before = UserRepository().count() 
+    count_profile_before = UserProfileRepository().count()
+
+    data = {
+        "name": "Test",
+        "surname": "Foo",
+        "email": "service_test@example.com",
+        "password": "test1234"
+    }
+
+    AuthenticationService().create_with_profile(**data)
+
+    assert UserRepository().count() == count_user_before + 1
+    assert UserProfileRepository().count() == count_profile_before + 1
+
 
 def test_service_create_with_profile_fail_no_email(clean_database):
     data = {
@@ -113,6 +129,20 @@ def test_service_create_with_profile_fail_no_password(clean_database):
     }
 
     with pytest.raises(ValueError, match="Password is required."):
+        AuthenticationService().create_with_profile(**data)
+
+    assert UserRepository().count() == 0
+    assert UserProfileRepository().count() == 0
+
+def test_service_campos_vacios(clean_database):
+    data = {
+        "name": "",
+        "surname": "",
+        "email": "",
+        "password": ""
+    }
+
+    with pytest.raises(ValueError, match="Email is required."):
         AuthenticationService().create_with_profile(**data)
 
     assert UserRepository().count() == 0
